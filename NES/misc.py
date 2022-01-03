edd82f3945d8744b4c298b483217ef3318749460
Serafim <s.grubas@g.nsu.ru>
2022-01-02 20:51:23 -0700

    added marmmousi model to 'data'

--- NES/misc.py
+++ NES/misc.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+from scipy.ndimage import gaussian_filter
+
 
 ######################################################
                 ### MISCELLANEOUS ###
@@ -159,3 +161,39 @@ class LocAnomaly:
         """ Computes the analytical gradient of velocity at 'X'
         """
         return (self.__call__(X) - self.vmin)[..., None] * (self.mus - X) / self.sigmas
+
+def Marmousi(smooth=None, section=None):
+    """
+        Creates Interpolator of Marmousi model
+
+        Arguments:
+            smooth : float : smoothes model using "scipy.ndimage.gaussian_filter(sigma=smooth)"
+                             If 'None', smoothing is not applied
+            section : list of ints : indices to cut out a rectangle part of Marmousi model. 
+                                     Example - 'section = [[100, 200], [0, 150]]' 
+                                     where the first pair is for axis 0, the second - axis 1
+        Return:
+            Vel : instance of 'NES.Interpolator' for Marmousi model in 'km/s' units
+    """
+
+    V = np.load('../data/Marmousi_Pwave_smooth_12.5m.npy') / 1000.0
+    if section is not None:
+        i = [0, V.shape[0]+1] if section[0] is None else section[0]
+        j = [0, V.shape[1]+1] if section[1] is None else section[1]
+        V = V[i[0] : i[1], j[0] : j[1]]
+    if smooth is not None:
+        V = gaussian_filter(V, sigma=smooth)
+
+    nx, nz = V.shape
+    xmin, xmax = 0.0, .0125 * nx
+    zmin, zmax = 0.0, .0125 * nz
+    x = np.linspace(xmin, xmax, nx)
+    z = np.linspace(zmin, zmax, nz)
+    Vel = Interpolator(V, x, z)
+    return Vel
+
+def MarmousiSmoothedPart():
+    """
+        Return smoothed central part of Marmousi model 'NES.Marmousi(smooth=3, section=[[600, 900], None])' 
+    """
+    return Marmousi(smooth=3, section=[[600, 900], [0, 10000]])
