895084244c5df702e5bdbbebe815d75c679cf0f5
Serafim <s.grubas@g.nsu.ru>
2022-01-06 00:53:50 -0700

    removed 'P' as function argument now 'p' is a power in Hamiltonian form

--- NES/eikonalLayers.py
+++ NES/eikonalLayers.py
@@ -11,26 +11,15 @@ class IsoEikonal(Layer):
             If 'int' or 'float', the exponentiation is applied. By default P=2.
             hamiltonian : boolean : whether to use hamiltonian form of the equation 'H = P(v * |grad tau|) - P(1)'. By default is True
     """
-    def __init__(self, P=3, hamiltonian=True, **kwargs):
-        """
-            Isotropic eikonal equation
-
-            Arguments:
-                P : callable or int or float : function for 'right hand side' and 'left hand side' of the equation. 
-                If 'int' or 'float', the exponentiation is applied. By default P=2.
-                hamiltonian : boolean : whether to use hamiltonian form of the equation 'H = P(v * |grad tau|) - P(1)'. By default is True
-        """
+    def __init__(self, p=3, hamiltonian=True, **kwargs):
+        if kwargs.get('name') is None:
+            kwargs['name'] = 'IsoEikonal'
         super(IsoEikonal, self).__init__(**kwargs)
         self.hamiltonian = hamiltonian
-        if isinstance(P, (float, int)):
-            if P != 0:
-                self.P = lambda x: pow(x, P) / P
-            else:
-                self.P = log
-        elif callable(P):
-            self.P = P
-        else:
-            assert False, "Unknown type of 'P' argument"
+
+        assert isinstance(p, (float, int)), " `p` must be float or int"
+        assert p != 0, "`p` must not be 0"
+        self.p = p
 
     def call(self, dT, v):
         eik = norm(concat(dT, axis=-1), axis=-1, keepdims=True)
@@ -42,10 +31,10 @@ class IsoEikonal(Layer):
             lhs = eik
             rhs = 1 / v
 
-        eikp = self.P(lhs) - self.P(rhs)
-        return eikp
+        eikp = pow(lhs, self.p) - pow(rhs, self.p)
+        return eikp / self.p
 
     def get_config(self):
         config = super(IsoEikonal, self).get_config()
-        config.update({"P": self.P, "hamiltonian": self.hamiltonian})
+        config.update({"p": self.p, "hamiltonian": self.hamiltonian})
         return config
