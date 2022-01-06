47b739417d2c1475df537819edd38a9250379b60
Serafim <s.grubas@g.nsu.ru>
2022-01-06 00:52:33 -0700

    retained only important modules in default import

--- NES/__init__.py
+++ NES/__init__.py
@@ -1,4 +1,5 @@
 from .NeuralEikonalSolver import NES_OP, NES_TP
-from .baseLayers import Activation, DenseBody, Diff, LossesHolder, Initializer, SourceLoc
+# from .baseLayers import Activation, DenseBody, Diff, LossesHolder, Initializer, SourceLoc
 from .eikonalLayers import IsoEikonal
-from .misc import Uniform_PDF, Interpolator, VerticalGradient, LocAnomaly, Marmousi, MarmousiSmoothedPart
+# from .misc import Uniform_PDF, Interpolator, VerticalGradient, LocAnomaly, Marmousi, MarmousiSmoothedPart
+from .misc import Uniform_PDF, Interpolator
