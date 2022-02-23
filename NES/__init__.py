2742061321848645ea7a22e6ccb532ad689bbc03
Serafim <s.grubas@g.nsu.ru>
2022-02-23 16:30:16 -0700

    minor

--- NES/__init__.py
+++ NES/__init__.py
@@ -1,5 +1,4 @@
 from .NeuralEikonalSolver import NES_OP, NES_TP
 from .utils import NES_EarlyStopping, LossesHolder, Generator
 from .eikonalLayers import IsoEikonal
-# from .misc import Uniform_PDF, Interpolator, VerticalGradient, LocAnomaly,  MarmousiSmoothedPart
 from .misc import Uniform_PDF, Interpolator
