5865503089e1203a2e830a6906f923529ffe2bfe
sgrubas <serafimgrubas@gmail.com>
2022-07-11 20:24:14 -0600

    misc file for back compatibility

--- NES/__init__.py
+++ NES/__init__.py
@@ -3,4 +3,5 @@ from .utils import NES_EarlyStopping, LossesHolder, Uniform_PDF, RegularGrid
 from .eikonalLayers import IsoEikonal
 from .velocity import Interpolator
 import NES.velocity
+import NES.misc
 import NES.ray_tracing
